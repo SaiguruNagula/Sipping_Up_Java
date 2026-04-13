43e62299d76978faeef05b945566f7c46e0f235e
SaiguruXircles <saigurunagula26@gmail.com>
2026-04-13 23:59:16 +0530

    MAp

--- Day08/maps.java
+++ Day08/maps.java
@@ -1,6 +1,26 @@
 package Day08;
 
 public class maps {
+    /**
+     * Demonstrates the basic usage of HashMap in Java.
+     * 
+     * This method showcases fundamental HashMap operations including:
+     * - Adding key-value pairs using put()
+     * - Retrieving values by key using get()
+     * - Iterating through all entries using entrySet()
+     * - Removing entries using remove()
+     * - Checking key existence using containsKey()
+     * 
+     * The example uses a HashMap to store fruit names (String keys) and their 
+     * quantities (Integer values), performing common map operations.
+     * 
+     * @param args Command line arguments (not used in this example)
+     * 
+     * Output:
+     * - Prints the count of apples
+     * - Displays all fruits and their quantities
+     * - Confirms the existence of apples after removal of bananas
+     */
     public static void main(String[] args) {
         // Example usage of HashMap
         java.util.Map<String, Integer> map = new java.util.HashMap<>();
